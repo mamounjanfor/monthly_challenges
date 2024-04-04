2073c0d47ab8083917ec4b1b38b355451593c2d2
Mamoun Janfor <Mamounjanfor@gmail.com>
2024-04-04 11:00:04 -0400

    adding views manually

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# reurns a http response says hello world when calling january
+def january(request):
+    return HttpResponse("eat no meat for the entire month of january")
+
+def february(request):
+    return HttpResponse("walk for at least 20 minutes every day")
+
+def march(request):
+    return HttpResponse("learn django for at least 20 minutes every day")
